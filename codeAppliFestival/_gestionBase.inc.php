a0d2c34b45a76a567c4c362e090e8212a7b0814d
sakina-zanati <sakina.zanati@gmail.com>
2017-09-14 14:17:59 +0200

    Modification _gestionBase
    Signed-off-by: sakina-zanati <sakina.zanati@gmail.com>

--- codeAppliFestival/_gestionBase.inc.php
+++ codeAppliFestival/_gestionBase.inc.php
@@ -8,16 +8,16 @@ function connect()
    $hote="localhost";
    $login="ts1";
    $mdp="ts1";
-   return mysql_connect($hote, $login, $mdp);
+   $bd="festival";
+   return mysqli_connect($hote, $login, $mdp, $bd);
 }
 
 function selectBase($connexion)
 {
-   $bd="festival";
+  
    $query="SET CHARACTER SET utf8";
    // Modification du jeu de caractères de la connexion
-   $res=mysql_query($query, $connexion); 
-   $ok=mysql_select_db($bd, $connexion);
+   $ok=mysqli_query($query, $connexion); 
    return $ok;
 }
 
@@ -46,14 +46,14 @@ function obtenirReqEtablissementsAyantChambresAttribuées()
 function obtenirDetailEtablissement($connexion, $id)
 {
    $req="select * from Etablissement where id='$id'";
-   $rsEtab=mysql_query($req, $connexion);
-   return mysql_fetch_array($rsEtab);
+   $rsEtab=mysqli_query($req, $connexion);
+   return mysqli_fetch_array($rsEtab);
 }
 
 function supprimerEtablissement($connexion, $id)
 {
    $req="delete from Etablissement where id='$id'";
-   mysql_query($req, $connexion);
+   mysqli_query($req, $connexion);
 }
  
 function modifierEtablissement($connexion, $id, $nom, $adresseRue, $codePostal, 
@@ -75,7 +75,7 @@ function modifierEtablissement($connexion, $id, $nom, $adresseRue, $codePostal,
          '$nomResponsable',prenomResponsable='$prenomResponsable',
          nombreChambresOffertes='$nombreChambresOffertes' where id='$id'";
    
-   mysql_query($req, $connexion);
+   mysqli_query($req, $connexion);
 }
 
 function creerEtablissement($connexion, $id, $nom, $adresseRue, $codePostal, 
@@ -95,15 +95,15 @@ function creerEtablissement($connexion, $id, $nom, $adresseRue, $codePostal,
          '$civiliteResponsable', '$nomResponsable', '$prenomResponsable',
          '$nombreChambresOffertes')";
    
-   mysql_query($req, $connexion);
+   mysqli_query($req, $connexion);
 }
 
 
 function estUnIdEtablissement($connexion, $id)
 {
    $req="select * from Etablissement where id='$id'";
-   $rsEtab=mysql_query($req, $connexion);
-   return mysql_fetch_array($rsEtab);
+   $rsEtab=mysqli_query($req, $connexion);
+   return mysqli_fetch_array($rsEtab);
 }
 
 function estUnNomEtablissement($connexion, $mode, $id, $nom)
@@ -120,15 +120,15 @@ function estUnNomEtablissement($connexion, $mode, $id, $nom)
    {
       $req="select * from Etablissement where nom='$nom' and id!='$id'";
    }
-   $rsEtab=mysql_query($req, $connexion);
-   return mysql_fetch_array($rsEtab);
+   $rsEtab=mysqli_query($req, $connexion);
+   return mysqli_fetch_array($rsEtab);
 }
 
 function obtenirNbEtab($connexion)
 {
    $req="select count(*) as nombreEtab from Etablissement";
-   $rsEtab=mysql_query($req, $connexion);
-   $lgEtab=mysql_fetch_array($rsEtab);
+   $rsEtab=mysqli_query($req, $connexion);
+   $lgEtab=mysqli_fetch_array($rsEtab);
    return $lgEtab["nombreEtab"];
 }
 
@@ -136,8 +136,8 @@ function obtenirNbEtabOffrantChambres($connexion)
 {
    $req="select count(*) as nombreEtabOffrantChambres from Etablissement where 
          nombreChambresOffertes!=0";
-   $rsEtabOffrantChambres=mysql_query($req, $connexion);
-   $lgEtabOffrantChambres=mysql_fetch_array($rsEtabOffrantChambres);
+   $rsEtabOffrantChambres=mysqli_query($req, $connexion);
+   $lgEtabOffrantChambres=mysqli_fetch_array($rsEtabOffrantChambres);
    return $lgEtabOffrantChambres["nombreEtabOffrantChambres"];
 }
 
@@ -161,8 +161,8 @@ function obtenirReqIdNomGroupesAHeberger()
 function obtenirNomGroupe($connexion, $id)
 {
    $req="select nom from Groupe where id='$id'";
-   $rsGroupe=mysql_query($req, $connexion);
-   $lgGroupe=mysql_fetch_array($rsGroupe);
+   $rsGroupe=mysqli_query($req, $connexion);
+   $lgGroupe=mysqli_fetch_array($rsGroupe);
    return $lgGroupe["nom"];
 }
 
@@ -172,8 +172,8 @@ function obtenirNomGroupe($connexion, $id)
 function existeAttributionsEtab($connexion, $id)
 {
    $req="select * From Attribution where idEtab='$id'";
-   $rsAttrib=mysql_query($req, $connexion);
-   return mysql_fetch_array($rsAttrib);
+   $rsAttrib=mysqli_query($req, $connexion);
+   return mysqli_fetch_array($rsAttrib);
 }
 
 // Retourne le nombre de chambres occupées pour l'id étab transmis
@@ -181,8 +181,8 @@ function obtenirNbOccup($connexion, $idEtab)
 {
    $req="select IFNULL(sum(nombreChambres), 0) as totalChambresOccup from
         Attribution where idEtab='$idEtab'";
-   $rsOccup=mysql_query($req, $connexion);
-   $lgOccup=mysql_fetch_array($rsOccup);
+   $rsOccup=mysqli_query($req, $connexion);
+   $lgOccup=mysqli_fetch_array($rsOccup);
    return $lgOccup["totalChambresOccup"];
 }
 
@@ -192,8 +192,8 @@ function modifierAttribChamb($connexion, $idEtab, $idGroupe, $nbChambres)
 {
    $req="select count(*) as nombreAttribGroupe from Attribution where idEtab=
         '$idEtab' and idGroupe='$idGroupe'";
-   $rsAttrib=mysql_query($req, $connexion);
-   $lgAttrib=mysql_fetch_array($rsAttrib);
+   $rsAttrib=mysqli_query($req, $connexion);
+   $lgAttrib=mysqli_fetch_array($rsAttrib);
    if ($nbChambres==0)
       $req="delete from Attribution where idEtab='$idEtab' and idGroupe='$idGroupe'";
    else
@@ -204,7 +204,7 @@ function modifierAttribChamb($connexion, $idEtab, $idGroupe, $nbChambres)
       else
          $req="insert into Attribution values('$idEtab','$idGroupe', $nbChambres)";
    }
-   mysql_query($req, $connexion);
+   mysqli_query($req, $connexion);
 }
 
 // Retourne la requête permettant d'obtenir les id et noms des groupes affectés
@@ -222,8 +222,8 @@ function obtenirNbOccupGroupe($connexion, $idEtab, $idGroupe)
 {
    $req="select nombreChambres From Attribution where idEtab='$idEtab'
         and idGroupe='$idGroupe'";
-   $rsAttribGroupe=mysql_query($req, $connexion);
-   if ($lgAttribGroupe=mysql_fetch_array($rsAttribGroupe))
+   $rsAttribGroupe=mysqli_query($req, $connexion);
+   if ($lgAttribGroupe=mysqli_fetch_array($rsAttribGroupe))
       return $lgAttribGroupe["nombreChambres"];
    else
       return 0;
